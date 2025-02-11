152b3367aff4e39462cc3a1fd769a76a9e20b8c9
Diego Garcia <garciatroisi@gmail.com>
2025-02-11 16:11:48 -0300

    totp generator

---
+++ agent-tools/totp-secret-generator/src/index.ts
@@ -0,0 +1,10 @@
+import * as speakeasy from "speakeasy";
+
+function generateSecretKey(agentId: string): string {
+  const secret = speakeasy.generateSecret({ name: `SafeRocket ${agentId}` });
+  console.log(`Secret key for ${agentId}: ${secret.base32}`);
+  return secret.base32;
+}
+
+const agentId: string = "agent1";
+generateSecretKey(agentId);
