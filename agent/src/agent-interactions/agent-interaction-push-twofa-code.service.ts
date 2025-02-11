09c5d4443775bae3dc19d6f92805e58ffbd67d29
Rodrigo Pintos <rpintos@spacedev.io>
2025-02-11 18:44:30 -0300

    before tackling the execute proposal

--- agent/src/agent-interactions/agent-interaction-push-twofa-code.service.ts
+++ agent/src/agent-interactions/agent-interaction-push-twofa-code.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { IAgentInteractionServiceInterface } from './agent-interaction.service.interface';
 import * as speakeasy from 'speakeasy';
 import { AgentConfiguration } from 'src/agent-configuration';
+import { AgentStateService } from 'src/agent-state.service';
 
 @Injectable()
 export class AgentInteractionPushTwoFAcodeService
@@ -9,14 +10,20 @@ export class AgentInteractionPushTwoFAcodeService
 {
   private readonly secret: string = AgentConfiguration.getTotp();
 
+  constructor(private readonly _agentStateService: AgentStateService) {}
+
   async performInteraction(token: string): Promise<string> {
+    if (!this._agentStateService.isThereAProposalWaitingForTwoFA())
+      return 'There is no operation waiting for 2fa';
     const isTokenValid = speakeasy.totp.verify({
       secret: this.secret,
       encoding: 'base32',
       token,
-      window: 1, // Allows for slight time drift
+      window: 1,
     });
-
+    if (isTokenValid) {
+      this._agentStateService.confirmTwoFA();
+    }
     return isTokenValid
       ? 'Valid code, operation successful'
       : 'Invalid code, operation failed';
