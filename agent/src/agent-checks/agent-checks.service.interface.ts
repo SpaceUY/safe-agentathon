acafbccd9700668c86d58332c5f52ef77b721658
Rodrigo Pintos <rpintos@spacedev.io>
2025-02-12 18:40:27 -0300

    impl completed have to test

--- agent/src/agent-checks/agent-checks.service.interface.ts
+++ agent/src/agent-checks/agent-checks.service.interface.ts
@@ -5,7 +5,13 @@ export interface ProposalTxs {
   operationName: string;
   multisigs: Multisig[];
   multisigTxs: MultisigTransaction[];
+  proposalTxs: ProposalTx[];
 }
+export interface ProposalTx {
+  multisig: Multisig;
+  multisigTx: MultisigTransaction;
+}
+
 export interface IAgentCheckServiceInterface {
   performCheck(proposalTxs: ProposalTxs): Promise<boolean>;
 }
