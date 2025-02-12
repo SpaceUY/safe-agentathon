05e1206d0934cc1b9b00597b5f1aab71b100704b
Rodrigo Pintos <rpintos@spacedev.io>
2025-02-12 17:25:34 -0300

    change how config is loaded

---
+++ agent/src/_common/config/agent2-config.ts
@@ -0,0 +1,32 @@
+export default {
+  id: 'AGENT2',
+  isMultisigExecutor: true,
+  autonomousProposalListener: true,
+  totp: 'EVMEKJB6LVKDUSC2ORYUGXTCIZETCTS5GZYSGRS5PU5GY3DCENSA',
+  multisigs: [
+    {
+      chainId: '84532',
+      address: '0x6fB0A20D6DefF6d6be127Fa86B754Ff12249631C',
+      rpcUrl: 'https://84532.rpc.thirdweb.com',
+    },
+    {
+      chainId: '11155111',
+      address: '0x6fB0A20D6DefF6d6be127Fa86B754Ff12249631C',
+      rpcUrl: 'https://11155111.rpc.thirdweb.com',
+    },
+  ],
+  txsToOperate: {
+    upgradeToAndCall: {
+      checks: ['upgrade-verification', 'upgrade-to-same-version-across-chains'],
+      chainIds: ['84532', '11155111'],
+      twoFARequired: true,
+      holdToCheck: true,
+      holdToReplicate: true,
+    },
+  },
+  interactions: [
+    'get-signer-address',
+    'get-operation-status',
+    'get-operation-details',
+  ],
+};
