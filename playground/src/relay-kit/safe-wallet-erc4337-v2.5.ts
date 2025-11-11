47f87238adbc99424fc0c4abe2e74d760b509945
Rodrigo Pintos <rpintos@spacedev.io>
2025-11-11 13:39:26 -0300

    adding how migrate from support v6 to v7

--- playground/src/relay-kit/safe-wallet-erc4337-v2.5.ts
+++ playground/src/relay-kit/safe-wallet-erc4337-v2.5.ts
@@ -237,16 +237,16 @@ const SAFE_4337_CUSTOM_CONTRACTS: Record<
     safeModulesVersion?: string;
   }
 > = {
-   '80002': {
-    safe4337ModuleAddress: '0x75cf11467937ce3f2f357ce24ffc3dbf8fd5c226',
-    safeModulesSetupAddress: '0x2dd68b007B46fBe91B9A7c3EDa5A7a1063cB5b47',
-    safeModulesVersion: '0.3.0',
-  },
-  // '80002': {
-  //   safe4337ModuleAddress: '0xa581c4A4DB7175302464fF3C06380BC3270b4037',
-  //   safeModulesSetupAddress: '0x8EcD4ec46D4D2a6B64fE960B3D64e8B94B2234eb',
-  //   safeModulesVersion: '0.2.0',
+  //  '80002': {
+  //   safe4337ModuleAddress: '0x75cf11467937ce3f2f357ce24ffc3dbf8fd5c226',
+  //   safeModulesSetupAddress: '0x2dd68b007B46fBe91B9A7c3EDa5A7a1063cB5b47',
+  //   safeModulesVersion: '0.3.0',
   // },
+  '80002': {
+    safe4337ModuleAddress: '0xa581c4A4DB7175302464fF3C06380BC3270b4037',
+    safeModulesSetupAddress: '0x8EcD4ec46D4D2a6B64fE960B3D64e8B94B2234eb',
+    safeModulesVersion: '0.2.0',
+  },
 };
 
 // Configuration
